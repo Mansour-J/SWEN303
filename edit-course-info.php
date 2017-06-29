8f0116f36fce0d28ff050f1f8cdf977415533878
Adrian Ng <beaconwarden@outlook.com>
2017-06-29 11:54:41 +0000

    finished 17 by jinsu

---
+++ edit-course-info.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Victoria University of Wellington">
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/selectize.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/vanillabox/vanillabox.css" type="text/css">
+
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+
+    <title>Victoria - Educational, Blog Post and University Template</title>
+
+</head>
+
+<body class="page-sub-page page-Blog Post-detail">
+<!-- Wrapper -->
+<div class="wrapper">
+<!-- Header -->
+<?php include 'header.php';?>
+<!-- end Header -->
+
+<!-- Breadcrumb -->
+<div class="container">
+    <ol class="breadcrumb">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="courses.php">Courses Homepages</a></li>
+        <li><a href="swen303-teacher.php">SWEN303 Home</a></li>
+        <li class="active">Edit Course Info</li>
+    </ol>
+</div>
+<!-- end Breadcrumb -->
+
+<!-- Page Content -->
+<div id="page-content">
+    <div class="container">
+        <header><h1>Edit SWEN303 Course Info</h1></header>
+        <div class="row">
+            <!--MAIN Content-->
+            <div class="col-md-2">
+            </div><!-- end Blog Post Image -->
+            <div class="col-md-8">
+                <div id="page-main">
+                    <section id="blog-post-detail">
+                        <article class="blog-post-detail">
+
+                            <section id="blog-post-detail-content">
+                                <div class="row">
+                                    <div >
+                                        <form role="form" class="clearfix" action="#">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="input-group">
+                                                        <div class="controls">
+                                                            <label for="message">Your Message</label>
+                                                            <textarea name="message" id="message" required="required" rows="10">This course addresses the design and engineering of user interfaces. It presents principles and guidelines for design and covers a range of design processes. It presents techniques for testing user interfaces, and considers a variety of user interface styles and interface devices. Students who pass this course should be able to: Understand principles of usability engineering; Design a user interface following accepted principles and guidelines; Evaluate a user interface design, and interpret the results to improve the design. Integrate user interface design techniques into the general software engineering life cycle.</textarea>
+                                                        </div><!-- /.controls -->
+                                                    </div><!-- /.control-group -->
+                                                </div><!-- /.col-md-4 -->
+                                            </div><!-- /.row -->
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="input-group">
+                                                        <div class="controls">
+                                                        <button type="submit" class="btn pull-right">Submit</button>
+                                                        <button type="button" class="btn pull-right">Reset</button>
+                                                        <button type="button" class="btn pull-right" onclick="window.location.href='/swen303-teacher.php'">Cancel</button>
+                                                        </div><!-- /.controls -->
+                                                    </div><!-- /.control-group -->
+                                                </div><!-- /.col-md-4 -->
+                                            </div><!-- /.row -->
+                                        </form>
+                                    </div>
+                                </div>
+                            </section><!-- /#Blog Post-content -->
+
+                        </article><!-- /.Blog Post-detail -->
+                    </section><!-- /.Blog Post-detail -->
+                </div><!-- /#page-main -->
+            </div><!-- /.col-md-8 -->
+            <!-- Blog Post Image -->
+            <div class="col-md-2">
+            </div><!-- end Blog Post Image -->
+
+     
+            <!-- end SIDEBAR Content-->
+        </div><!-- /.row -->
+    </div><!-- /.container -->
+</div>
+<!-- end Page Content -->
+
+<!-- Footer -->
+<?php include 'footer.php';?>
+<!-- end Footer -->
+
+</div>
+<!-- end Wrapper -->
+
+<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/selectize.min.js"></script>
+<script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.placeholder.js"></script>
+<script type="text/javascript" src="assets/js/jQuery.equalHeights.js"></script>
+<script type="text/javascript" src="assets/js/icheck.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.vanillabox-0.1.5.min.js"></script>
+<script type="text/javascript" src="assets/js/countdown.js"></script>
+<script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+
+<script type="text/javascript" src="assets/js/custom.js"></script>
+
+</body>
+</html>
